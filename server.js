026c1fed2dbd61380899de1c71f68cc9eb39084b
bpiekars <47162704+bpiekars@users.noreply.github.com>
2019-04-23 00:52:18 -0400

    Add database connection and required packages

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-var express = require('express');
+var express = require('express'); // Express is a web application framework for NodeJs
 var pug = require('pug');
-var path = require('path');
-var app = express();
-var bodyParser = require('body-parser');
+var path = require('path'); // Node Js path module provides waty
+var bodyParser = require('body-parser'); // Express package body-parser parses http post request body
 var methodOverride = require('method-override');
-//var mongoose = require('mongoose');
+var cookieParser = require('cookie-pasrer'); // package that generates and stores local cookies
+var logger = require('morgan'); // Express HTTP logger middleware
+var passport = require('passport'); // package used for user authentication in express
+var mongoose = require('mongoose'); // Mongoose package required to work with MongoDB
+/* var routes = require('./routes/index');
+var users = require('./routes/users'); */
 
+var app = express(); // create new express application
 //var db = require('./config/db');
 
 var port = process.env.PORT || 8080;
@@ -16,7 +21,16 @@ app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'public'))
 
+// connect to database
+var url = 'mongodb://localhost:27017'; // global variable for database location
+mongoose.connect(url);
 //mongoose.connect(db.url);
+// test if mongoose connection to mongodb server is open, print log to console indicating status
+mongoose.connection.once('open', function(){
+	console.log("Connected to MongoDB!");
+}).on('error', function(error){
+	console.log('Connection error:', error);
+});
 
 /*app.get('/', function(req, res) {
     res.render('log.html');
