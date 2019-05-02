23da899d8ed3b3dca0beb7e724111a93f1fc695e
bpiekars <47162704+bpiekars@users.noreply.github.com>
2019-05-02 14:30:52 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -17,22 +17,7 @@ var UserSchema = new mongoose.Schema({
     required: true
   },
 	timeBalance: { type: Number, default: 24 },
-	id: { type: Number }
 });
-
-// Generate salt and hash for password
-UserSchema.methods.generateHash = function (password){
-	return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
-};
-
-// Check if password is valid by loading hash from db and checking if entered password equals unhashed password
-UserSchema.methods.comparePassword = function(password){
-	return bcrypt.compareSync(password, this.password);
-}
-
-/* UserSchema.methods.comparePassword = function(plaintext, callback) {
-    return callback(null, bcrypt.compare(plaintext, this.password));
-}; */
 
 // Export User database model
 module.exports = mongoose.model('User', UserSchema);
