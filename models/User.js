df1654f088960e8e7a5ca2c3917bc75d86b6b2ea
bpiekars <47162704+bpiekars@users.noreply.github.com>
2019-05-02 14:31:43 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,22 +1,30 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
 
 // Database schema for a user
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    useCreateIndex: true,
-    required: true,
-    trim: true
-  },
-	firstname: { type: String, required: true },
-	lastname: { type: String, required: true },
-	location: String,
-  password: {
-    type: String,
-    required: true
-  },
-	timeBalance: { type: Number, default: 24 },
+    email: {
+        type: String,
+        useCreateIndex: true,
+        required: true,
+        trim: true
+    },
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    location: String,
+    password: {
+        type: String,
+        required: true
+    },
+    timeBalance: {
+        type: Number,
+        default: 24
+    },
 });
 
 // Export User database model
